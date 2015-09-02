74824369b96eaa5cd1794763b8e987c40a592d7d
Jurek Kedra <jurek.kedra@gmail.com>
2015-09-02 20:58:40 +0200

    loading gmaps without iframe

--- static/pokazMape.js
+++ static/pokazMape.js
@@ -8,8 +8,38 @@ function regPokazMape(buttonSel, buttonId, targetId, mapLink) {
 		});
 }
 
-function regPokazMapy() {
 
+function regPokazMape2(buttonSel, buttonId, targetId, mapLink) {
+    $(buttonSel+"#"+buttonId).on("click",
+		function() {
+			$(this).fadeOut( 1000 );
+			//$("#"+targetId).attr("src", mapLink);
+			//$("#"+targetId).show( 1000 );
+			$("#"+targetId).show();
+
+			var mapOptions = {
+				//zoom: 13,
+				//center: {lat: 49.489603, lng: 20.196834 },
+				scrollwheel: false,
+				zoomControl: true,
+				zoomControlOptions: {
+				  style: google.maps.ZoomControlStyle.SMALL,
+				  position: google.maps.ControlPosition.RIGHT_BOTTOM
+				}
+			}
+
+			var map = new google.maps.Map(document.getElementById(targetId), mapOptions,
+					{visibility: true});
+
+			var ctaLayer = new google.maps.KmlLayer({
+				url: mapLink,
+				map: map
+			});
+
+	});
+}
+
+function regPokazMapy() {
     regPokazMape('p.pokazMape', 'tatry',
         'map_tatry',
 		'https://www.google.com/maps/d/u/0/embed?mid=zBqI9lALHG00.kSinWvHQ1vTQ');
@@ -18,14 +48,12 @@ function regPokazMapy() {
         'map_starawies',
 		'https://mapsengine.google.com/map/u/0/embed?mid=zBqI9lALHG00.kO7CEoIiN8oM');
 
-    regPokazMape('p.pokazMape', 'ochotnica',
-        'map_ochotnica',
-		'https://www.google.com/maps/d/u/0/embed?mid=zBqI9lALHG00.kFrN6dxyNvsI');
+    regPokazMape2('p.pokazMape2', 'ochotnica',
+        'map_ochotnica', 'http://kedra.org/kml/Ochotnica.kmz');
 
     regPokazMape('p.pokazMape', 'muszyna',
         'map_muszyna',
 		'https://mapsengine.google.com/map/u/0/embed?mid=zBqI9lALHG00.kTnm-73ulm0Y');
-
 }
 
 $(document).ready(regPokazMapy);
