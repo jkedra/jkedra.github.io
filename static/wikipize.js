89b31e7d683b83d5fdc3f23019c92e7482954da4
Jurek Kedra <jurek.kedra@gmail.com>
2016-11-02 19:11:27 +0100

    refactoring

--- static/wikipize.js
+++ static/wikipize.js
@@ -1,72 +1,80 @@
+var DEBUG=false;
 
+function log(str) {
+	if(DEBUG) console.log(str);
+}
 
-function wikipize() {
-  console.log("WIKIPIZE");
+function wikipize(lang, entry) {
+ log("WIKIPIZE");
+  //  w:xxx  - polish wiki
+  //  we:xxx - english wiki
+  switch(lang) {
+    case undefined:
+      lang = "pl"; break;
+    case "e":
+      lang = "en"; break;
+  }
+  // if empty link, use <A>TEXT</A>
+  if(entry.length==0)
+    entry = $(this).text();
+
+  return("https://"+lang+".wikipedia.org/wiki/"+entry);
+}
+
+function googlize(extra, entry) {
+  log("GOOGLIZE");
+  //  g:xxx  - search on google
+  //  gp:xxx - search on google pictures
+  switch(extra) {
+    case undefined:
+       extra = "";          break;
+    case "p":
+       extra = "&tbm=isch"; break;
+   }
+   
+   return("https://www.google.com/search?q="+entry+extra);
+
+}
+
+function ize() {
+  log("IZE");
   $("a").each(
     function() {
       var href = $(this).attr("href");
+      //  g:xxx  - search on google
+      //  gp:xxx - search on google pictures
+	  //
       //  w:xxx  - polish wiki
       //  we:xxx - english wiki
       //
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
       // http://www.rexegg.com/regex-conditionals.html
       // optionally enclosed in ()
-      var results = /^w(\w)*:(.*)/.exec( href )
+      var results = /^([gw])(\w)*:(.*)/.exec( href )
       if(results != null) {
-        var lang  = results[1]; // wiki language
-        var entry = results[2]; // wiki entry
-        switch(lang) {
-          case undefined:
-            lang = "pl"; break;
-          case "e":
-            lang = "en"; break;
-        }
+		var typize = results[1]; // link type w or g
+        var extra  = results[2]; // google service, add extra tags to search string
+        var entry  = results[3]; // search tag
+
         // if empty link, use <A>TEXT</A>
         if(entry.length==0)
           entry = $(this).text();
 
-        targetLink = "https://"+lang+".wikipedia.org/wiki/"+entry;
-
-        console.log("HREF=" + href + " => " + targetLink);
-        $(this).attr("href", targetLink);
-      }
-  });
-}
-
-function googlize() {
-  console.log("GOOGLIZE");
-  $("a").each(
-    function() {
-      var href = $(this).attr("href");
-      //  g:xxx  - search on google
-      //  gp:xxx - search on google pictures
-      //
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
-      // http://www.rexegg.com/regex-conditionals.html
-      // optionally enclosed in ()
-      var results = /^g(\w)*:(.*)/.exec( href )
-      if(results != null) {
-        var extra = results[1]; // google service, add extra tags to search string
-        var entry = results[2]; // search tag
-        switch(extra) {
+        switch(typize) {
           case undefined:
             extra = "";          break;
-          case "p":
-            extra = "&tbm=isch"; break;
+          case "g":
+            targetLink = googlize(extra, entry).replace(/\s+/g, '+');
+		    break;
+          case "w":
+            targetLink = wikipize(extra, entry).replace(/\s+/g, '_');
+            break;
         }
-        // if empty link, use <A>TEXT</A>
-        if(entry.length==0)
-          entry = $(this).text();
 
-        targetLink = "https://www.google.com/search?q="+entry+extra;
-
-        console.log("HREF=" + href + " => " + targetLink);
+        log("HREF=" + href + " => " + targetLink);
         $(this).attr("href", targetLink);
       }
   });
 }
 
-
-
-$(document).ready(wikipize);
-$(document).ready(googlize);
+$(document).ready(ize);
