2f1fddf4524a5770f44d7318ba5d1e11c66d683a
Kedra, Jurek <jurek.kedra@gmail.com>
2021-05-27 12:51:34 +0200

    jekyll issues

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 require 'json'
 require 'open-uri'
-versions = JSON.parse(URI.open('https://pages.github.com/versions.json').read)
+# versions = JSON.parse(URI.open('https://pages.github.com/versions.json').read)
 
-gem 'github-pages', versions['github-pages']
+# gem 'github-pages', versions['github-pages'], group: :jekyll_plugins
+gem "github-pages", "~> 214", group: :jekyll_plugins
 gem 'eventmachine'
--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # http://jekyllrb.com/docs/github-pages/
 # https://help.github.com/articles/using-jekyll-with-pages/#installing-jekyll
-# gem install bundler
+# gem install jekyll bundler
 
-if [ $(hostname) == 'Ubuntu16a' ]; then
-    echo Hello Ubuntu16a
-    export https_proxy=http://127.0.0.1:3128/
+if [ $(hostname) == 'ubuntu20' ]; then
+    echo Hello ubuntu20
+    #export https_proxy=http://127.0.0.1:3128/
 fi
-bundle update
+#bundle update
 #bundle exec jekyll serve --drafts --watch
 
 # Settings in later files override settings in earlier files.
